4e1cc3b83c439aeb0364451ed4b0e0702ca1a4ef
alexrr04 <alex.542004@gmail.com>
2025-05-10 12:46:22 +0200

    chore: remove unused files and update .gitignore for VSCode settings

--- client/scripts/config.ts
+++
@@ -1,9 +0,0 @@
-// API configuration
-export const SKYSCANNER_API_KEY = 'sh967490139224896692439644109194';
-export const SKYSCANNER_API_URL =
-  'https://partners.api.skyscanner.net/apiservices/v3/flights/indicative/search';
-
-export default {
-  SKYSCANNER_API_KEY,
-  SKYSCANNER_API_URL,
-};
